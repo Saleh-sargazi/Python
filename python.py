c5ddd286f45d2572c548882e4285e3e2dc666063
Saleh-sargazi <sal.sargazi@gmail.com>
2024-06-25 13:22:05 +0330

    py

--- python.py
+++ python.py
@@ -1,5 +1,8 @@
-name = "saleh"
-lastname = " sargazi"
-city = " im live in  tehran"
-me = name + lastname + city
-print(me)
+#name = "saleh"
+#lastname = " sargazi"
+#city = " im live in  tehran"
+ #me = name + lastname + city
+ #print(me)
+age = 16
+txt = "My name is John, I am " + str(age)
+print(txt) 
